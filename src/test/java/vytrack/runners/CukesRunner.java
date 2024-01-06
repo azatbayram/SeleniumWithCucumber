087ee60586a1f39caa7240462de92bab8f830ec0
azatbayram <bayramov.azat.tkm@gmail.com>
2024-01-06 08:28:57 +0300

    Navigation menu feature file is created

--- src/test/java/vytrack/runners/CukesRunner.java
+++ src/test/java/vytrack/runners/CukesRunner.java
@@ -12,7 +12,7 @@ import org.junit.runner.RunWith;
         features = "src/test/resources/features",
         glue = "vytrack/step_definitions",
         dryRun = false,
-        tags = " @driver"
+        tags = " @sales_manager"
 )
 public class CukesRunner {
 }
